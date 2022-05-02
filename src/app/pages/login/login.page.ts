e1a29fd10fc3bbf42f4b2e69beaaa4349a422d9f
Tony Melling <tonymelling76@gmail.com>
2022-05-02 02:53:02 +0100

    Landing Page 1st design

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor() { /* TODO document why this constructor is empty */  }
 
   ngOnInit() {
+    // TODO document why this method 'ngOnInit' is empty
+
   }
 
 }
