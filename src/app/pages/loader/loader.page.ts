d1077adac7e8661de8e33e2c26ac5c5c9254be31
Tony Melling <tonymelling76@gmail.com>
2022-05-02 01:41:02 +0100

    First Changes

---
+++ src/app/pages/loader/loader.page.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loader',
+  templateUrl: './loader.page.html',
+  styleUrls: ['./loader.page.scss'],
+})
+export class LoaderPage implements OnInit {
+
+  constructor() { /* TODO document why this constructor is empty */  }
+
+  ngOnInit() {
+    // TODO document why this method 'ngOnInit' is empty
+  
+  }
+
+}
